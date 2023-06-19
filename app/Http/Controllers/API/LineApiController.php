c715ec87cbb34f8e8c1eb58f8de1e7c1048b9949
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-06-19 17:40:50 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -61,7 +61,11 @@ class LineApiController extends Controller
         Image::make($binary_data)->save($new_path);
 
         $image = Image::make( storage_path('app/public').'/uploads/ocr/'.$filename );
-        $image->resize(500, 500);
+        // $image->resize(500, 500);
+        $image->resize(400, 400, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
         $watermark = Image::make( public_path('img/logo/green-logo-01.png') );
         $watermark->resize(100, 100);
         $image->insert($watermark ,'bottom-right', 50, 50);
