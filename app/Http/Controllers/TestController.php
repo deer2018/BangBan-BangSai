12f4eb7ecb8b0492ac900a4b17505e08c8dbf00e
Lomtale <43232280+deer2018@users.noreply.github.com>
2023-06-20 14:11:23 +0700

    ทดสอบปรับขนาดรูป_3

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,93 +11,92 @@ use App\Models\Mylog;
 class TestController extends Controller
 {
 
-    public function store()
+    public function store(Request $request)
     {
+        //SAVE LOG
+        $requestData = $request->all();
+        $data = [
+            "title" => "Line",
+            "content" => json_encode($requestData, JSON_UNESCAPED_UNICODE),
+        ];
+        MyLog::create($data);
 
-    	$text_json = '{"destination":"Ubf0ea4dab738442af3d0d9092c62ac5f","events":[{"type":"message","message":{"type":"image","id":"458666893059293493","contentProvider":{"type":"line"}},"webhookEventId":"01H2D6VWJFHAXZE3VSAQZDFGFB","deliveryContext":{"isRedelivery":false},"timestamp":1686218076739,"source":{"type":"user","userId":"Ua561f9244840375d1d97d7550d22fb68"},"replyToken":"b93c5b87e0dd42bfa10e15ab5631429f","mode":"active"}]}';
+        //GET ONLY FIRST EVENT
+        $event = $requestData["events"][0];
 
-    	$sss = json_decode($text_json, true);
-    	$event = $sss["events"][0];
-
-    	// dd($event);
-    	// echo "<pre>";
-    	// print_r($event);
-    	// echo "<pre>";
-
-    	//LOAD REMOTE IMAGE AND SAVE TO LOCAL
-        $binary_data  = $this->getImageFromLine($event["message"]["id"]);
-        $filename = $this->random_string(20).".png";
-        $new_path = storage_path('app/public').'/uploads/ocr/'.$filename;
-
-        $image_data = base64_encode(file_get_contents($new_path));
-
-        Image::make($binary_data)->save($new_path);
-
-    	// $image = Image::make(public_path('img/พื้นหลัง/พื้นหลัง-05.png'));
-
-	    // $watermark_2 = Image::make( public_path('img/logo/green-logo-01.png') );
-	    // $image->insert($watermark_2 ,'bottom-right', 385, 150);
-
-     //    $filename = $this->random_string(10).".png";
-     //    $image->save( public_path('img/พื้นหลัง/' . $filename) );
-
-
-
-        return view('welcome');
+        switch($event["type"]){
+            case "message" :
+                if($event["message"]["type"] == "image"){
+                    $this->image_convert($event);
+                }else{
+                    $this->messageHandler($event);
+                }
+                break;
+            case "postback" :
+                // $this->postbackHandler($event);
+                break;
+            case "join" :
+                // $this->save_group_line($event);
+                break;
+            // case "follow" :
+            //     $this->user_follow_line($event);
+            //     DB::table('users')
+            //         ->where([
+            //                 ['type', 'line'],
+            //                 ['provider_id', $event['source']['userId']],
+            //                 ['status', "active"]
+            //             ])
+            //         ->update(['add_line' => 'Yes']);
+            //     break;
+        }
     }
 
-    public function image_convert()
+
+    public function image_convert($event)
     {
-        $text_json = '{
-            "destination": "Ubf0ea4dab738442af3d0d9092c62ac5f",
-            "events": [
-                {
-                    "type": "message",
-                    "message": {
-                        "type": "text",
-                        "id": "459507626582475378",
-                        "text": "……."
-                    },
-                    "webhookEventId": "01H2W4RQYCFZKXTVFNTZJY754K",
-                    "deliveryContext": {
-                        "isRedelivery": false
-                    },
-                    "timestamp": 1686719192588,
-                    "source": {
-                        "type": "user",
-                        "userId": "Ua561f9244840375d1d97d7550d22fb68"
-                    },
-                    "replyToken": "d3504663c20d482d9dc40796442e38e4",
-                    "mode": "active"
-                }
-            ]
-        }';
+        //LOAD REMOTE IMAGE AND SAVE TO LOCAL
+        $binary_data  = $this->getImageFromLine($event["message"]["id"]);
+        // $filename = $this->random_string(20).".png";
+        // $new_path = storage_path('app/public').'/uploads/ocr/'.$filename;
 
-    	$sss = json_decode($text_json, true);
-    	$event = $sss["events"][0];
-        // //LOAD REMOTE IMAGE AND SAVE TO LOCAL
-        // $binary_data  = $this->getImageFromLine($event["message"]["id"]);
+        // Image::make($binary_data)->save($new_path);
 
-        $img = Image::make('https://img.freepik.com/free-vector/hand-painted-watercolor-pastel-sky-background_23-2148902771.jpg');
-        $img->resize(1080 , 1920);
-        $filename = $this->random_string(20).".png";
-        $new_path = storage_path('app/public').'/uploads/ocr/'.$filename;
-
-        Image::make($img)->save($new_path);
-
-        $image = Image::make( storage_path('app/public').'/uploads/ocr/'.$filename );
+        // $image = Image::make( storage_path('app/public').'/uploads/ocr/'.$filename );
+        $image = Image::make( storage_path('app/public').'/img/พื้นหลัง/ChartBG.png');
+        $image->resize(1080,1920);
         $watermark = Image::make( public_path('img/logo/green-logo-01.png') );
-        $image->insert($watermark ,'bottom-right', 385, 150);
+        $watermark->resize(250, 250);
+        $image->insert($watermark ,'top-right', 10, 10);
+
+        // define polygon points
+        $points = [
+            240,  260,  // Point 1 (x, y)
+            260,  260, // Point 2 (x, y)
+            260,  450,  // Point 3 (x, y)
+            240, 450  // Point 4 (x, y)
+        ];
+
+        // draw a filled blue polygon with red border
+        $image->polygon($points, function ($draw) {
+            $draw->background('#0000ff');
+            $draw->border(1, '#ff0000');
+        });
+
         $image->save();
 
-        // Convert image data to base64
-        // $image_data = base64_encode(file_get_contents($new_path));
+        // $template_path = storage_path('../public/json/flex_img.json');
+        // $string_json = file_get_contents($template_path);
+
+        // $string_json = str_replace("ตัวอย่าง" , 'ส่งรูปภาพ' ,$string_json);
+        // $string_json = str_replace("FILENAME" , $filename ,$string_json);
+
+        // $messages = [ json_decode($string_json, true) ];
 
         $messages = [
             [
                 'type' => 'image',
-                'originalContentUrl' => 'https://www.mithcare.com/'.$image, // Replace with the URL of the image to send
-                'previewImageUrl' => 'https://www.mithcare.com/'.$image, // Replace with the URL of a preview image
+                'originalContentUrl' => 'https://bangban-bangsai.viicheck.com/storage/uploads/ocr/'.$image, // เปลี่ยน URL นี้ให้เป็น URL ของรูปภาพที่ต้องการส่ง
+                'previewImageUrl' => 'https://bangban-bangsai.viicheck.com/storage/uploads/ocr/'.$image, // เปลี่ยน URL นี้ให้เป็น URL ของรูปภาพตัวอย่างก่อนการแสดง
             ]
         ];
 
@@ -106,18 +105,18 @@ class TestController extends Controller
             "messages" => $messages,
         ];
 
-        $content = json_encode($body);
-
         $opts = [
             'http' =>[
                 'method'  => 'POST',
                 'header'  => "Content-Type: application/json \r\n".
                             'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
-                'content' => $content,
+                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+                //'timeout' => 60
             ]
         ];
 
         $context  = stream_context_create($opts);
+        //https://api-data.line.me/v2/bot/message/11914912908139/content
         $url = "https://api.line.me/v2/bot/message/reply";
         $result = file_get_contents($url, false, $context);
 
@@ -128,8 +127,8 @@ class TestController extends Controller
         ];
         MyLog::create($data);
         return $result;
-    }
 
+    }
 
     public function getImageFromLine($id){
         $opts = array('http' =>[
@@ -157,5 +156,80 @@ class TestController extends Controller
         }
 
         return $key;
+    }
+
+    public function messageHandler($event)
+    {
+        switch($event["message"]["type"]){
+            case "text" :
+                $this->textHandler($event);
+                break;
+        }
+
+    }
+
+    public function textHandler($event)
+    {
+
+        switch( strtolower($event["message"]["text"]) ){
+            case "ระดับน้ำ" :
+                $this->replyToUser(null, $event, "water level");
+                break;
+            // default :
+            //     $this->replyToUser(null, $event, $event["message"]["text"]);
+            //     break;
+        }
+    }
+
+    public function replyToUser($data, $event, $message_type)
+    {
+        switch($message_type)
+        {
+            case 'water level':
+                $template_path = storage_path('../public/json/quick_reply.json');
+                $string_json = file_get_contents($template_path);
+
+                $messages = [ json_decode($string_json, true) ];
+            break;
+            // case $message_type:
+            //     $template_path = storage_path('../public/json/text.json');
+            //     $string_json = file_get_contents($template_path);
+
+            //     $string_json = str_replace("ตัวอย่าง" , $message_type ,$string_json);
+            //     $string_json = str_replace("เปลี่ยนข้อความตรงนี้" , $message_type ,$string_json);
+
+            //     $messages = [ json_decode($string_json, true) ];
+            // break;
+        }
+
+        $body = [
+            "replyToken" => $event["replyToken"],
+            "messages" => $messages,
+        ];
+
+        $opts = [
+            'http' =>[
+                'method'  => 'POST',
+                'header'  => "Content-Type: application/json \r\n".
+                            'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
+                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+                //'timeout' => 60
+            ]
+        ];
+
+        $context  = stream_context_create($opts);
+        //https://api-data.line.me/v2/bot/message/11914912908139/content
+        $url = "https://api.line.me/v2/bot/message/reply";
+        $result = file_get_contents($url, false, $context);
+
+        //SAVE LOG
+        $data = [
+            "title" => "reply Success",
+            "content" => "message quickReply",
+        ];
+        MyLog::create($data);
+
+        return $result;
+
     }
 }
